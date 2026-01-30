5d58c2d049f96666ab34b444b21bad0934706528
YutoImata <yuto.imata.wine@gmail.com>
2026-01-31 02:43:30 +0900

    feat: Tagの定数クラスを作成

---
+++ Assets/GGJ/Scripts/InGame/Constant/Tags.cs
@@ -0,0 +1,11 @@
+namespace GGJ.Core
+{
+    /// <summary>
+    /// プロジェクト全体で使用するTag名の定数
+    /// </summary>
+    public static class Tags
+    {
+        public const string NPC = "Npc";
+        public const string Player = "Player";
+    }
+}
