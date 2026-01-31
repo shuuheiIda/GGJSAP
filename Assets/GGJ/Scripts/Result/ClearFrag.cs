96f2cbbeac099b0af44e28ee8f25adfbc3a59598
Yuki-Osafune <opi1994@hotmail.com>
2026-01-31 18:44:28 +0900

    resultv2

--- Assets/GGJ/Scripts/Result/ClearFrag.cs
+++ Assets/GGJ/Scripts/Result/ClearFrag.cs
@@ -10,16 +10,21 @@ namespace GGJ.Result
     {
     [SerializeField] public GameObject Win;//今はテキストを格納だが、のちにイメージファイルに変えてもよい。
     [SerializeField] public GameObject Lose;
-    [SerializeField] public GameObject Time;
-    [SerializeField] public bool Result = false;//勝ちでtrue.負けでfalse.
-    [SerializeField] public int ResultTime = 0;
-    [SerializeField] public TMP_Text ScoreText;
-
+    [SerializeField] public GameObject Time;//経過した時間（ResultTime:）のテキストオブジェクトを格納
+    [SerializeField] public GameObject UseHint;//ヒントを聞いた数（UseHint:）のテキストオブジェクトを格納
+    //[SerializeField] public GameObject ReturnTitle;
+    [SerializeField] public bool Result = false;//勝ちでtrue.負けでfalse.結果も別スクリプトから引っ張る。
+    [SerializeField] public int ResultTime = 0;//この数字、ゲームクリア時に経過した時間を入れる。別のスクリプトから引っ張ってくる必要あり
+    [SerializeField] public int UseHintPoint = 0;//この数字、NPCから情報を聞いた数が反映される予定。
+    [SerializeField] public TMP_Text ScoreText;//経過した時間（ResultTime:）のテキストオブジェクトを格納
+    [SerializeField] public TMP_Text UseHintText;//ヒントを聞いた数（UseHint:）のテキストオブジェクトを格納
         void Awake()
         {
+            //
             Win.SetActive(false);
             Lose.SetActive(false);
             Time.SetActive(false);
+            UseHint.SetActive(false);
         }
 
         // Start is called before the first frame update
@@ -29,12 +34,14 @@ namespace GGJ.Result
             {
                 Win.SetActive(true);
                 Time.SetActive(true);
+                UseHint.SetActive(true);
             }else
             {
                 Lose.SetActive(true);
             }
 
-             ScoreText.text = "ResultTime:" + ResultTime;
+            ScoreText.text = "ResultTime:" + ResultTime;
+            UseHintText.text = "UseHint:" + UseHintPoint;
         }
 
         // Update is called once per frame
---
+++ Assets/GGJ/Scripts/Result/FadeIn.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+namespace GGJ.Result
+{
+    
+    public class FadeIn : MonoBehaviour
+{
+    [SerializeField] private Image fadeImage; // 黒い画像
+    [SerializeField] private float duration = 0.1f; // フェード時間
+    private bool isFading = false;
+
+    private void Start()
+    {
+        // シーン開始時にフェードイン
+        StartCoroutine(FadeInCoroutine());//FadeInCoroutineを実行
+    }
+    private void Update()
+    {
+        // スペースキーが押されたらフェードアウト開始
+        if (Input.GetKeyDown(KeyCode.Space) && !isFading)
+        {
+            StartCoroutine(FadeOutCoroutine());
+        }
+    }
+
+
+    private IEnumerator FadeInCoroutine()
+    {
+        isFading = true;
+
+        float t = 0f;
+        Color c = fadeImage.color;
+        c.a = 1f;
+        fadeImage.color = c;
+
+        while (t < duration)//tが duration（フェード時間）に到達するまで繰り返す
+        {
+            t += Time.deltaTime;　
+            c.a = 1f - (t / duration);
+            fadeImage.color = c;
+            yield return null;
+        }
+
+        // 最後に完全に透明にしておく
+        c.a = 0f;
+        fadeImage.color = c;
+        isFading = false;
+
+    }
+
+    private IEnumerator FadeOutCoroutine()
+    {
+        isFading = true;
+
+        float t = 0f;
+        Color c = fadeImage.color;
+        c.a = 0f;
+        fadeImage.color = c;
+
+        while (t < duration)
+        {
+            t += Time.deltaTime;
+            c.a = t / duration;
+            fadeImage.color = c;
+            yield return null;
+        }
+
+        c.a = 1f;
+        fadeImage.color = c;
+
+        isFading = false;
+        
+    }
+
+}
+
+}
