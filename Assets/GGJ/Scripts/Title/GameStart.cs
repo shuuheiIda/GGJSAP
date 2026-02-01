c5cdeae85dc68924e37a3bc0246c4139f27a17e9
YutoImata <yuto.imata.wine@gmail.com>
2026-02-01 15:02:46 +0900

    feat: タイトルに音量調節バーを追加

--- Assets/GGJ/Scripts/Title/GameStart.cs
+++ Assets/GGJ/Scripts/Title/GameStart.cs
@@ -1,27 +1,13 @@
-using System;
+using GGJ.InGame.Audio;
 using UnityEngine;
-using UnityEngine.InputSystem;
-using UnityEngine.SceneManagement;
 
 namespace GGJ.Title
 {
     public class GameStart : MonoBehaviour
     {
-        // Start is called before the first frame update
         void Start()
         {
-        
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-            {
-                if (Input.GetKeyDown(KeyCode.Space)) // スペースキーが押されたら
-                {
-                SceneManager.LoadScene(""); // シーンを切り替える。該当シーンはめどが立ったら追加。
-                }
-            }
+            AudioManager.I.PlayBGM(BGMType.Title);
         }
     }
 }
